68a7c132b8f699e86a106b4d32f0060deea7e99c
haramkim-1 <kimharam@gmail.com>
2017-11-14 14:14:11 -0500

    even more mlis

--- client.mli
+++ client.mli
@@ -1,9 +1,13 @@
+(*client will contain the polling REPL; poll every 2 seconds and update gui*)
+
 
 (*hang the client until a update is pushed from the server
  * Should probably have a timeout time in case connection is lost
+ Try serversent events possibly?
 *)
 val wait_for_update : unit -> unit
 
 (*send an action to the server*)
-val send_action : actions -> unit
+val send_action : action -> unit
 
+(*val init_connection : *)
--- gui.mli
+++ gui.mli
@@ -1,11 +1,12 @@
 type country = String
 
 (*Variant representing actions the user can take*)
-type actions =
+(*type action =
   | APlace_Troops of (country * int) list
   | AAttack of country * country
   | AFinish_Attacking
   | AMove_Troops of (country * country) * int
+*)
 
 type state
 
