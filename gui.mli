125daac3b7944ceda92e8b7b2b8f07c52d6c95f4
Sam Ringel <SamJRingel@gmail.com>
2017-11-13 22:44:50 -0500

    updated gui mli a bit

--- gui.mli
+++ gui.mli
@@ -1,2 +1,13 @@
+type country = String
+
+(*Variant representing actions the user can take*)
+type actions =
+  | APlace_Troops of (country * int) list
+  | AAttack of country * country
+  | AFinish_Attacking
+  | AMove_Troops of (country * country) * int
+
+type state
+
 (*Updates the gui to reflect *)
 val update : state -> unit
