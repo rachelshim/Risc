edb34e06d1b45caf3d9745e9b3f0cc63074f2a58
Avani Bhargava <avanib@optonline.net>
2017-11-15 22:17:57 -0500

    server mli and updates to client mli

--- client.mli
+++ client.mli
@@ -13,6 +13,25 @@ val polling_repl : unit -> unit
   JSON format. *)
 val send_action : action -> unit
 
+(* [create_game req] starts up a game; it updates the game state with the
+   information that the game is in progress; it returns a response containing
+   the initial, empty map state. *)
+val create_game : request -> response
+
+(* [join_game req] takes in a JSON request containing a player username and
+   updates the game state with the new user; it returns a response containing
+   the new state of user info. *)
+val join_game : request -> response
+
+(** [move req] takes in a JSON request containing information about a player
+    move: the type of action and the regions and number of troops involved *)
+val move : request -> response
+
+(* [quit_game req] updates the game state with the information that the game is
+   no longer in progress; it returns a response containing the winner. *)
+val quit_game : request -> response
+
+
 (** Start up the client by prompting for a username and IP to connect to. Once
     provided, connect to the server using the provided IP and call the
     main function that runs the GUI. *)
