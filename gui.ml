2d8eed4d234c2c0eb5e92605ff83b58b03acd3a1
haramkim-1 <kimharam@gmail.com>
2017-11-22 20:02:33 -0500

    setup map

--- gui.ml
+++ gui.ml
@@ -1,29 +1,31 @@
 open GMain
 open GdkKeysyms
+open Gtk
+open GtkBase
+open GtkMain
 open Unix
 open Thread
+open GnomeCanvas
+open GnoCanvas
 
 let locale = GtkMain.Main.init ()
+
+let map_pixbuf = GdkPixbuf.from_file "resources/map.png"
 
 let mymutex = Core.Mutex.create ()
 
 let myfunction () =
   Mutex.lock mymutex;
-  Unix.sleep 5;
+  (*Unix.sleep 5;*)
   prerr_endline "lock/unlock";
   Mutex.unlock mymutex
 
-let rec heartbeat () = 
-  print_endline "beat";
-  Unix.sleep 1;
-  heartbeat ();
-  ()
-
 let main () =
-  let window = GWindow.window ~width:320 ~height:240
-                                    ~title:"Simple lablgtk program" () in
+  let window = GWindow.window ~width:1400 ~height:700
+                                    ~title:"Risc" () in
     let vbox = GPack.vbox ~packing:window#add () in
-    let a = window#connect#destroy ~callback:Main.quit in
+    let a = window#connect#destroy ~callback:Main.quit; in
+
 
       (* Menu bar *)
       let menubar = GMenu.menu_bar ~packing:vbox#pack () in
@@ -36,14 +38,21 @@ let main () =
         let b = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in
 
           (* Button *)
+          (*)
           let button = GButton.button ~label:"Push me!"
                                             ~packing:vbox#add () in
-            let c = button#connect#clicked ~callback: (myfunction) in
+          let c = button#connect#clicked ~callback: (myfunction) in
+          *)
+          let canvas = GnoCanvas.canvas                               
+                                        ~packing:vbox#add () in
+          let canvas_background = GnoCanvas.pixbuf ~x:0. ~y:50. ~pixbuf:map_pixbuf ~props:[ `ANCHOR `CENTER ] canvas#root in  
+
+            (*disable button example*)
+            (*button#misc#set_sensitive false;*)  
 
               (* Display the windows and enter Gtk+ main loop *)
               window#add_accel_group accel_group;
               window#show ();
               Main.main ()
 
-let thr = Thread.create heartbeat ()
 let () = main ()
