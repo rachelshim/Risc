2c5b7f02001d37b56fbfd024f07d4f2422b6f441
haramkim-1 <kimharam@gmail.com>
2017-11-23 23:47:58 -0500

    not working

--- gui.ml
+++ gui.ml
@@ -7,6 +7,10 @@ open Unix
 open Thread
 open GnomeCanvas
 open GnoCanvas
+open GdkPixbuf
+open GtkWindow
+open Widget
+open Gdk.Color
 
 let locale = GtkMain.Main.init ()
 
@@ -20,16 +24,27 @@ let myfunction () =
   prerr_endline "lock/unlock";
   Mutex.unlock mymutex
 
+let draw_background ev =
+  let widget_window = GdkEvent.get_window ev in
+  (*let widget_gc = Gdk.GC.create widget_window in
+  GdkPixbuf.draw_pixbuf widget_window widget_gc map_pixbuf 
+                        ~width:1227 ~height:600 ~dest_x:50 ~dest_y:50;*)
+
+  let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
+  Gdk.Window.set_back_pixmap widget_window (`PIXMAP pmap);
+  true
+
 let main () =
-  let window = GWindow.window ~width:1400 ~height:700
+  let window = GWindow.window ~width:1500 ~height:800
                                     ~title:"Risc" () in
     let a = window#connect#destroy ~callback:Main.quit; in
-    let vbox = GPack.vbox ~packing:window#add () in
+    let fixed = GPack.fixed ~packing:window#add () in
     
 
-
+    let signal = fixed#event#connect#expose ~callback:(draw_background) in
+    
       (* Menu bar *)
-      let menubar = GMenu.menu_bar ~packing:vbox#pack () in
+      let menubar = GMenu.menu_bar ~packing:(fixed#add) () in
         let factory = new GMenu.factory menubar in
         let accel_group = factory#accel_group in
         let file_menu = factory#add_submenu "File" in
@@ -37,23 +52,20 @@ let main () =
         (* File menu *)
         let factory = new GMenu.factory file_menu ~accel_group in
         let b = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in
+      
 
           (* Button *)
-          (*)
           let button = GButton.button ~label:"Push me!"
-                                            ~packing:vbox#add () in
+                                      ~packing:(fixed#put ~x:0 ~y:0) () in
           let c = button#connect#clicked ~callback: (myfunction) in
-          *)
-          let canvas = GnoCanvas.canvas                               
-                                        ~packing:vbox#add () in
-          let canvas_background = GnoCanvas.pixbuf ~x:0. ~y:50. ~pixbuf:map_pixbuf ~props:[ `ANCHOR `CENTER ] canvas#root in  
-
-            (*disable button example*)
-            (*button#misc#set_sensitive false;*)  
+          
+          (*disable button example*)
+          (*button#misc#set_sensitive false;*)  
 
               (* Display the windows and enter Gtk+ main loop *)
               window#add_accel_group accel_group;
               window#show ();
+
               Main.main ()
 
 let () = main ()
