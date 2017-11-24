cee2c5e7fa8e3545fd4a7cbc3b85296a496dad62
haramkim-1 <kimharam@gmail.com>
2017-11-24 03:02:21 -0500

    more polish and setup button framework

--- gui.ml
+++ gui.ml
@@ -4,8 +4,8 @@ open Gtk
 open Unix
 open Thread
 
+(*GTK globals setup*)
 let locale = GtkMain.Main.init ()
-
 let map_pixbuf = GdkPixbuf.from_file "resources/map.png"
 
 let mymutex = Core.Mutex.create ()
@@ -17,54 +17,68 @@ let myfunction x =
   Mutex.unlock mymutex;
   true
 
+let add_button (pack:GPack.fixed) x y tooltip1 tooltip2 = 
+  let button = GButton.button ~label:"0"
+  ~packing:(pack#put ~x:x ~y:y) () in
+  GtkData.Tooltips.set_tip (GtkData.Tooltips.create ()) button#as_widget 
+                          ~text:tooltip1 ~privat:tooltip2;
+  let button_signal = button#event#connect#button_press 
+                          ~callback: (myfunction) in
+  ()
+
 let main () =
-  let window = GWindow.window ~width:1600 ~height:650
+  let window = GWindow.window ~width:1600 ~height:960
                               ~title:"Risc" ~resizable:false () in
-    let window_exit_signal = window#connect#destroy ~callback:Main.quit; in
-    let pane_pack = GPack.paned ~width:1600 ~height:650 
-                                ~packing:window#add ~border_width:5 
-                                `HORIZONTAL () in
+  let window_exit_signal = window#connect#destroy ~callback:Main.quit; in
 
-    let gameplay_pack = GPack.fixed ~has_window:true ~width:1227 ~height:650 
-                    ~packing:(pane_pack#pack1 ~resize:false ~shrink:false) () in
+  let top_pane_pack = GPack.paned ~width:1600 ~height:960 
+                              ~packing:window#add ~border_width:5 
+                              `VERTICAL () in
 
-    let sidebar_pack = GPack.paned ~width:370 ~height:650 ~border_width:5 
-                    ~packing:(pane_pack#pack2 ~resize:false ~shrink:false)
-                    `VERTICAL () in
+  let log_pack = GPack.vbox ~width:1590 ~height:300 
+                  ~packing:(top_pane_pack#pack2 ~resize:false ~shrink:false)
+                  () in
 
-    let info_pack = GPack.vbox ~width:360 ~height:240 
-                    ~packing:(sidebar_pack#pack1 ~resize:false ~shrink:false)
-                    () in
+  let bottom_pane_pack = GPack.paned ~width:1600 ~height:650 
+                  ~packing:(top_pane_pack#pack1 ~resize:false ~shrink:false) 
+                  ~border_width:5 `HORIZONTAL () in
 
-    let cards_pack = GPack.vbox ~width:360 ~height:400 
-                    ~packing:(sidebar_pack#pack2 ~resize:false ~shrink:false)
-                    () in
+  let gameplay_pack = GPack.fixed ~has_window:true ~width:1227 ~height:650 
+                  ~packing:(bottom_pane_pack#pack1 ~resize:false ~shrink:false) () in
 
-      (* Menu bar *)
-      let menubar = GMenu.menu_bar ~packing:(gameplay_pack#add) () in
-        let factory = new GMenu.factory menubar in
-        let accel_group = factory#accel_group in
-        let file_menu = factory#add_submenu "File" in
+  let sidebar_pack = GPack.paned ~width:370 ~height:650 ~border_width:5 
+                  ~packing:(bottom_pane_pack#pack2 ~resize:false ~shrink:false)
+                  `VERTICAL () in
 
-        (* File menu *)
-        let factory = new GMenu.factory file_menu ~accel_group in
-        let file_quit_signal = factory#add_item "Quit" ~key:_Q 
-                                      ~callback: Main.quit in
-      
-          (* Button *)
-          let button = GButton.button ~label:"0"
-                        ~packing:(gameplay_pack#put ~x:68 ~y:60) () in
-          let a = GtkData.Tooltips.set_tip (GtkData.Tooltips.create ()) button#as_widget ~text:"Alaska" ~privat:"Part of North America" in
-          let button_signal = button#event#connect#button_press ~callback: (myfunction) in
-          
+  let info_pack = GPack.vbox ~width:360 ~height:240 
+                  ~packing:(sidebar_pack#pack1 ~resize:false ~shrink:false)
+                  () in
 
-            (* Display the windows and enter Gtk+ main loop *)
-            window#add_accel_group accel_group;
-            window#show ();
+  let cards_pack = GPack.vbox ~width:360 ~height:400 
+                  ~packing:(sidebar_pack#pack2 ~resize:false ~shrink:false)
+                  () in
 
-            let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
-            Gdk.Window.set_back_pixmap gameplay_pack#misc#window (`PIXMAP pmap);
+  (*Menu bar creation*)
+  let menubar = GMenu.menu_bar ~packing:(gameplay_pack#add) () in
+  let factory = new GMenu.factory menubar in
+  let accel_group = factory#accel_group in
+  let file_menu = factory#add_submenu "File" in
 
-            Main.main ()
+  (*File menu setup *)
+  let factory = new GMenu.factory file_menu ~accel_group in
+  let file_quit_signal = factory#add_item "Quit" ~key:_Q 
+                                ~callback: Main.quit in
+
+  (*Territory button setup*)
+  add_button gameplay_pack 68 60 "Alaska" "Part of North America";
+
+  (*Final window configuration and display*)
+  window#add_accel_group accel_group;
+  window#show ();
+  let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
+  Gdk.Window.set_back_pixmap gameplay_pack#misc#window (`PIXMAP pmap);
+
+  (*main GTK loop*)
+  Main.main ()
 
 let () = main ()
