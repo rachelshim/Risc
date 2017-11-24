6a28d4c11f80fe9eb1213dc2ae98550339455a89
haramkim-1 <kimharam@gmail.com>
2017-11-24 00:52:20 -0500

    basic idea working

--- gui.ml
+++ gui.ml
@@ -24,24 +24,11 @@ let myfunction () =
   prerr_endline "lock/unlock";
   Mutex.unlock mymutex
 
-let draw_background ev =
-  let widget_window = GdkEvent.get_window ev in
-  (*let widget_gc = Gdk.GC.create widget_window in
-  GdkPixbuf.draw_pixbuf widget_window widget_gc map_pixbuf 
-                        ~width:1227 ~height:600 ~dest_x:50 ~dest_y:50;*)
-
-  let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
-  Gdk.Window.set_back_pixmap widget_window (`PIXMAP pmap);
-  true
-
 let main () =
   let window = GWindow.window ~width:1500 ~height:800
                                     ~title:"Risc" () in
     let a = window#connect#destroy ~callback:Main.quit; in
-    let fixed = GPack.fixed ~packing:window#add () in
-    
-
-    let signal = fixed#event#connect#expose ~callback:(draw_background) in
+    let fixed = GPack.fixed ~has_window:true ~width:500 ~height:500 ~packing:window#add () in
     
       (* Menu bar *)
       let menubar = GMenu.menu_bar ~packing:(fixed#add) () in
@@ -53,10 +40,11 @@ let main () =
         let factory = new GMenu.factory file_menu ~accel_group in
         let b = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in
       
-
           (* Button *)
           let button = GButton.button ~label:"Push me!"
-                                      ~packing:(fixed#put ~x:0 ~y:0) () in
+                                      ~packing:(fixed#put ~x:0 ~y:0)
+                                      (*~packing:(table#attach ~left:0 ~top:0)*)
+                                      (*~packing:(bbox#add)*) () in
           let c = button#connect#clicked ~callback: (myfunction) in
           
           (*disable button example*)
@@ -65,6 +53,9 @@ let main () =
               (* Display the windows and enter Gtk+ main loop *)
               window#add_accel_group accel_group;
               window#show ();
+
+              let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
+              Gdk.Window.set_back_pixmap fixed#misc#window (`PIXMAP pmap);
 
               Main.main ()
 
