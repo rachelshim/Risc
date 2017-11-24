6f2553bbbc93671df5522698226a1b4544bcd483
haramkim-1 <kimharam@gmail.com>
2017-11-24 02:47:37 -0500

    polish + tooltip

--- gui.ml
+++ gui.ml
@@ -10,19 +10,36 @@ let map_pixbuf = GdkPixbuf.from_file "resources/map.png"
 
 let mymutex = Core.Mutex.create ()
 
-let myfunction () =
+let myfunction x =
   Mutex.lock mymutex;
   (*Unix.sleep 5;*)
   prerr_endline "lock/unlock";
-  Mutex.unlock mymutex
+  Mutex.unlock mymutex;
+  true
 
 let main () =
-  let window = GWindow.window ~width:1500 ~height:800
-                                    ~title:"Risc" () in
-    let a = window#connect#destroy ~callback:Main.quit; in
-    let pane_pack = GPack.paned `HORIZONTAL ~width:1500 ~height:800 () in
-    let gameplay_pack = GPack.fixed ~has_window:true ~width:1230 ~height:650 ~packing:window#add () in
-    
+  let window = GWindow.window ~width:1600 ~height:650
+                              ~title:"Risc" ~resizable:false () in
+    let window_exit_signal = window#connect#destroy ~callback:Main.quit; in
+    let pane_pack = GPack.paned ~width:1600 ~height:650 
+                                ~packing:window#add ~border_width:5 
+                                `HORIZONTAL () in
+
+    let gameplay_pack = GPack.fixed ~has_window:true ~width:1227 ~height:650 
+                    ~packing:(pane_pack#pack1 ~resize:false ~shrink:false) () in
+
+    let sidebar_pack = GPack.paned ~width:370 ~height:650 ~border_width:5 
+                    ~packing:(pane_pack#pack2 ~resize:false ~shrink:false)
+                    `VERTICAL () in
+
+    let info_pack = GPack.vbox ~width:360 ~height:240 
+                    ~packing:(sidebar_pack#pack1 ~resize:false ~shrink:false)
+                    () in
+
+    let cards_pack = GPack.vbox ~width:360 ~height:400 
+                    ~packing:(sidebar_pack#pack2 ~resize:false ~shrink:false)
+                    () in
+
       (* Menu bar *)
       let menubar = GMenu.menu_bar ~packing:(gameplay_pack#add) () in
         let factory = new GMenu.factory menubar in
@@ -31,25 +48,23 @@ let main () =
 
         (* File menu *)
         let factory = new GMenu.factory file_menu ~accel_group in
-        let b = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in
+        let file_quit_signal = factory#add_item "Quit" ~key:_Q 
+                                      ~callback: Main.quit in
       
           (* Button *)
-          let button = GButton.button ~label:"Push me!"
-                                      ~packing:(gameplay_pack#put ~x:0 ~y:0)
-                                      (*~packing:(table#attach ~left:0 ~top:0)*)
-                                      (*~packing:(bbox#add)*) () in
-          let c = button#connect#clicked ~callback: (myfunction) in
+          let button = GButton.button ~label:"0"
+                        ~packing:(gameplay_pack#put ~x:68 ~y:60) () in
+          let a = GtkData.Tooltips.set_tip (GtkData.Tooltips.create ()) button#as_widget ~text:"Alaska" ~privat:"Part of North America" in
+          let button_signal = button#event#connect#button_press ~callback: (myfunction) in
           
-          (*disable button example*)
-          (*button#misc#set_sensitive false;*)  
 
-              (* Display the windows and enter Gtk+ main loop *)
-              window#add_accel_group accel_group;
-              window#show ();
+            (* Display the windows and enter Gtk+ main loop *)
+            window#add_accel_group accel_group;
+            window#show ();
 
-              let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
-              Gdk.Window.set_back_pixmap gameplay_pack#misc#window (`PIXMAP pmap);
+            let pmap = GdkPixbuf.create_pixmap map_pixbuf |> fst in
+            Gdk.Window.set_back_pixmap gameplay_pack#misc#window (`PIXMAP pmap);
 
-              Main.main ()
+            Main.main ()
 
 let () = main ()
