df7b25e44e40f8d0c25f825510a3a02c4287c29a
haramkim-1 <kimharam@gmail.com>
2017-12-03 18:15:46 -0500

    fix for action mli

--- action.mli
+++ action.mli
@@ -2,7 +2,7 @@ type action =
 | Deploy of string
 | Reinforce of string * int
 | Move of (string * string) * int
-| Attack of string * string
+| Attack of (string * string) * int
 | TradeSameInf
 | TradeSameCav
 | TradeSameArt
