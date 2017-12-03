62a62185eeae9d0ed80766f3682d76840e25d993
Avani Bhargava <avanib@optonline.net>
2017-12-02 23:04:55 -0500

    controller started

--- action.mli
+++ action.mli
@@ -3,6 +3,6 @@ type action =
 | Reinforce of string * int
 | Move of (string * string) * int
 | Attack of string * string
-| Trade_cards_different
-| Trade_cards_same 
-| End_turn
+| TradeSame
+| TradeDiff
+| EndTurn
---
+++ controller.ml
@@ -0,0 +1,11 @@
+open States
+open Action
+
+let update_gui act =
+  ()
+
+let update act st =
+  st
+
+let init_state num =
+  failwith "Unimplemented"
