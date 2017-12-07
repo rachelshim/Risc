52e04f5ca1ad27144cf0c7c7c40cfb72588018ba
Sam Ringel <SamJRingel@gmail.com>
2017-12-07 00:34:07 -0500

    added some init state tests

--- state_test.ml
+++ state_test.ml
@@ -10,9 +10,18 @@ let six_player = init_state 6
 
 let tests =
 [
-  (* Tests on two-player game *)
+  (* Init tests on two-player game *)
   "two_player_init" >:: (fun _ -> assert_equal (player_id (current_player two_player)) "Red");
-
+  "tp_cards1" >::  (fun _ -> assert_equal ((two_player |> current_player |> num_inf) + (two_player |> current_player |> num_cav) + (two_player |> current_player |> num_art) + (two_player |> current_player |> num_wild)) 0);
+  "tp_troops" >:: (fun _ -> assert_equal (avail_troops two_player) 19);
+  "tp_regions" >:: (fun _ -> assert_equal
+                               (List.fold_left
+                                 (fun (p1, p2) r ->
+                                   if ctrl_of_reg two_player r = "Red"
+                                   then (p1 + 1, p2)
+                                   else (p1, p2 + 1))
+                                 (0, 0)
+                                 (get_regions two_player)) (21, 21))
 
 ]
 
