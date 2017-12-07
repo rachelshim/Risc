87b6faecf928fd94c99a1d68c6a2d64f125f581b
Rachel Shim <cs899@cornell.edu>
2017-12-06 21:22:55 -0500

    fixing makefile and improving testing format

--- state_test.ml
+++ state_test.ml
@@ -2,15 +2,17 @@ open OUnit2
 open State
 open Action
 
+let two_player = init_state 2
+let three_player = init_state 3
+let four_player = init_state 4
+let five_player = init_state 5
+let six_player = init_state 6
+
 let tests =
 [
-  "format" >:: (fun _ -> assert_equal "jej" ("jej"));
+  (* Tests on two-player game *)
+  "two_player_init" >:: (fun _ -> assert_equal (player_id (current_player two_player)) "Red");
 
-  (** Initialize state *)
-  "init_state" >:: (fun _ -> assert_equal
-                       (player_id (current_player (init_state 2)))
-                       "Red"
-                   );
 ]
 
 let suite = "State tests" >::: tests
